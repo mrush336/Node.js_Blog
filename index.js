c76a48c6454fe42a3fc48246180cfc104a28c872
Matthew Rushford <49460419+mrush336@users.noreply.github.com>
2020-01-04 14:48:35 -0500

    Initial commit for this project.

---
+++ index.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const express = require("express");
+const app = new express();
+
+app.use(express.static("public"));
+app.use(express.static("pages"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+});
+
+app.get("/post", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/post.html"));
+});
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000");
+});
