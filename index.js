60c91f4bcccddd3c1a9a90705f9d87e95396ebaf
mrush336 <mrush336@gmail.com>
2020-01-05 13:02:10 -0500

    Adding logging middleware on express server.

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const path = require("path");
 const express = require("express");
 const app = new express();
 
+app.use((req, res, next) => {
+  console.log('Time: ', + Date.now());
+  next();
+});
 app.use(express.static("public"));
 app.use(express.static("pages"));
+
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "pages/index.html"));
